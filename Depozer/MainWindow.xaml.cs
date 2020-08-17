f0ca8bb6016cec9e686b904f7d810f69f16cf805
Cam Downs <1cysw0rdk0@gmail.com>
2020-08-17 13:23:03 -0400

    Channel Selection functional

--- Depozer/MainWindow.xaml.cs
+++ Depozer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,14 +19,72 @@ namespace Depozer {
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
+
+		List<SelectableChannelItem> Channels;
+
 		public MainWindow() {
 			InitializeComponent();
 
 			// Call to save the powershell logs to file
-			WevtapiHandler.ExportChannel(IntPtr.Zero, EventLogChannel.PowerShell, "C:\\Users\\camdo\\Desktop\\test.evtx");
+			//WevtapiHandler.ExportChannel(IntPtr.Zero, EventLogChannel.PowerShell, "C:\\Users\\camdo\\Desktop\\test.evtx");
+
+			GetSystemConfiguration();
 
 
+		}
 
+		private void GetSystemConfiguration() {
+			EventLog[] channels_raw = WevtapiHandler.EnumerateChannels();
+			Channels = new List<SelectableChannelItem>();
+
+			foreach (EventLog channel in channels_raw ) {
+				SelectableChannelItem NewChannel = new SelectableChannelItem();
+				NewChannel.Selected = false;
+				NewChannel.Channel = channel;
+				NewChannel.ChannelName = channel.LogDisplayName;
+
+				Channels.Add(NewChannel);
+			}
+
+
+			ChannelsDisplay.ItemsSource = Channels;
+
+
+		}
+
+		private class SelectableChannelItem {
+			bool selected;
+			EventLog channel;
+			string channelName;
+
+			public bool Selected {
+				get { return (selected); }
+				set { selected = value;  }
+			}
+
+			public string ChannelName {
+				get { return channelName; }
+				set { channelName = value;  }
+			}
+
+			public EventLog Channel {
+				get { return channel; }
+				set { channel = value; }
+			}
+		}
+
+		private void ChannelsAll_Click(object sender, RoutedEventArgs e) {
+			foreach (SelectableChannelItem channel in Channels) {
+				channel.Selected = true;
+				ChannelsDisplay.Items.Refresh();
+			}
+		}
+
+		private void ChannelsNone_Click(object sender, RoutedEventArgs e) {
+			foreach (SelectableChannelItem channel in Channels) {
+				channel.Selected = false;
+				ChannelsDisplay.Items.Refresh();
+			}
 		}
 	}
 }
